7c9ff431f78bd02a25a46c3a1aed74400e2189b4
Hermes Garcia <hermesgarcia@macbook-hermes.local>
2021-08-16 13:13:29 -0500

    Fix mail send script

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,23 +1,29 @@
 <?php
-error_reporting(-1);
-ini_set('display_errors', 'On');
-set_error_handler("var_dump");
-/*
-	PHP contact form script
-	Version: 1.0
-	Hermes Garcia
-	info@hermesgarcia.com
-*/
+/**
+ * PHP contact form script
+ * Version: 2.0
+ * Hermes Garcia
+ * info@hermesgarcia.com
+ */
+
+require '../vendor/autoload.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 /***************** Configuration *****************/
-$contactEmailTo = 'info@hermesgarcia.com';
-$contactEmailFrom = 'contacto@hermesgarcia.com';
-$ccEmail = 'hermes@hermesgarcia.com';
-$bccEmail = 'hgarciamanzanarez@gmail.com';
+//SMTP Config
+$SMTPHost = '';
+$SMTPUsername = '';
+$SMTPPassword = '';
+//Mailing Config
+$contactEmailFrom = '';
+$contactEmailTo = '';
+$ccEmail = '';
 $subjectTitle = 'Mensaje desde hermesgarcia.com';
 $nameTitle = 'Nombre:';
 $emailTitle = 'Email:';
 $messageTitle = 'Mensaje:';
-
+//Error Config
 $contactErrorName = 'El nombre es muy corto o muy largo';
 $contactErrorEmail = 'Por favor ingresa un email válido';
 $contactErrorSubject = 'El asunto es muy corto o muy largo';
@@ -41,7 +47,7 @@ if(isset($_POST)) {
 	$subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
 
-	if(!$contactEmailTo || $contactEmailTo == 'contact@example.com') {
+	if($contactEmailTo == '') {
 		die('El email de recepción para el formulario de contacto no ha sudo configurado');
 	}
 
@@ -64,25 +70,40 @@ if(isset($_POST)) {
 	if(!isset($contactEmailFrom)) {
 		$contactEmailFrom = 'contacto@' . @preg_replace('/^www\./','', $_SERVER['SERVER_NAME']);
 	}
-
-	$headers = 'From: ' . $name . ' <' . $contactEmailFrom . '>' . PHP_EOL;
-	$headers .= 'Reply-To: ' . $email . PHP_EOL;
-	$headers .= 'Cc: ' . $ccEmail  . PHP_EOL;
-	$headers .= 'Bcc: ' . $bccEmail . PHP_EOL;
-	$headers .= 'MIME-Version: 1.0' . PHP_EOL;
-	$headers .= 'Content-Type: text/html; charset=UTF-8' . PHP_EOL;
-	$headers .= 'X-Mailer: PHP/' . phpversion();
-
-	$message_content = '<strong>' . $nameTitle . '</strong> ' . $name . '<br>';
-	$message_content .= '<strong>' . $emailTitle . '</strong> ' . $email . '<br>';
-	$message_content .= '<strong>' . $messageTitle . '</strong> ' . nl2br($message);
-
-	//$sendemail = mail($contactEmailTo, $subjectTitle . ' ' . $subject, $message_content, $headers);
-	$sendemail = true;
-
-	if( $sendemail ) {
-        echo "OK";
-	} else {
-        echo 'El email no pudo ser enviado, intentálo de nuevo.';
+	
+	$mail = new PHPMailer(true);
+	
+	try {
+		$mail->isSMTP();
+		$mail->Host       = $SMTPHost;
+		$mail->SMTPAuth   = true;
+		$mail->Username   = $SMTPUsername;
+		$mail->Password   = $SMTPPassword;
+		$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+		$mail->Port       = 465;
+		$mail->setFrom($contactEmailFrom, $name);
+		$mail->addReplyTo($email);
+		$mail->addAddress($contactEmailTo);
+		if($ccEmail != ''){
+			$mail->addCC($ccEmail);
+		}
+		if(isset($bccEmail) && $bccEmail != ''){
+			$mail->addBCC($bccEmail);
+		}
+		
+		$message_content = '<strong>' . $nameTitle . '</strong> ' . $name . '<br>';
+		$message_content .= '<strong>' . $emailTitle . '</strong> ' . $email . '<br>';
+		$message_content .= '<strong>' . $messageTitle . '</strong> ' . nl2br($message);
+		
+		$mail->isHTML(true);
+		$mail->Subject = $subjectTitle . ' ' . $subject;
+		$mail->Body    = $message_content;
+		$mail->AltBody = $message_content;
+		
+		$mail->send();
+		echo 'OK';
+	} catch (Exception $e) {
+		echo 'El mensaje no pudo ser enviado, intentalo de nuevo.';
 	}
+	
 }
